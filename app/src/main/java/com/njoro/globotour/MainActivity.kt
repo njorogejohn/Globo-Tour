867072368e9843e35f84824dd29be3f003bc2d9a
njorojohn <johnnjoroge40@gmail.com>
2021-07-28 02:32:50 +0300

    setup the main actitvity

--- app/src/main/java/com/njoro/globotour/MainActivity.kt
+++ app/src/main/java/com/njoro/globotour/MainActivity.kt
@@ -2,10 +2,25 @@ package com.njoro.globotour
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.njoro.globotour.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var navController: NavController
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.activityMainToolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
