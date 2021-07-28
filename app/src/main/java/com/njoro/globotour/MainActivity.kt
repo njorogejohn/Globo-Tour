c5c419b8e44544c134dc09ee4b585cc8102978cf
njorojohn <johnnjoroge40@gmail.com>
2021-07-28 10:45:31 +0300

    setup the navigatonview and the drawerlayout

--- app/src/main/java/com/njoro/globotour/MainActivity.kt
+++ app/src/main/java/com/njoro/globotour/MainActivity.kt
@@ -2,15 +2,21 @@ package com.njoro.globotour
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 import com.njoro.globotour.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var toolbar: MaterialToolbar
     private lateinit var navController: NavController
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +28,14 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController = navHostFragment.navController
+
+        //Define appBarConfiguration: connect drawerlayout with Navigation component
+        val appBarConfiguration = AppBarConfiguration(navController.graph,binding.drawerLayout)
+
+        //connect toolbar with navcontroller
+        binding.activityMainToolbar.setupWithNavController(navController,appBarConfiguration)
+
+        //connect navigationview with navcontroller
+        binding.navView.setupWithNavController(navController)
     }
 }
