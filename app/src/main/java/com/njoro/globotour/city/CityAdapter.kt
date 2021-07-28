e94d87c2db534521b818014237fb97f5592f0120
njorojohn <johnnjoroge40@gmail.com>
2021-07-28 21:51:51 +0300

    mark favorite as well as delete citites

--- app/src/main/java/com/njoro/globotour/city/CityAdapter.kt
+++ app/src/main/java/com/njoro/globotour/city/CityAdapter.kt
@@ -1,7 +1,6 @@
 package com.njoro.globotour.city
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,34 +11,40 @@ import com.njoro.globotour.R
 import com.njoro.globotour.city.CityAdapter.CityViewHolder
 import com.njoro.globotour.databinding.ListItemCityBinding
 
-class CityAdapter(val context: Context, var cityList: ArrayList<City>): Adapter<CityViewHolder>() {
+class CityAdapter(val context: Context, var cityList: ArrayList<City>) : Adapter<CityViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
-        val listItemCityBinding = ListItemCityBinding.inflate(LayoutInflater.from(context), parent, false)
+        val listItemCityBinding =
+            ListItemCityBinding.inflate(LayoutInflater.from(context), parent, false)
         return CityViewHolder(listItemCityBinding.root)
     }
 
     override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
         val city = cityList[position]
         holder.setData(city, position)
+        holder.setListeners()
+
     }
 
     override fun getItemCount(): Int = cityList.size
 
 
-    inner class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
 
         private var currentPosition: Int = -1
         private var currentCity: City? = null
-        private var binding =  ListItemCityBinding.bind(itemView)
+        private var binding = ListItemCityBinding.bind(itemView)
         private val icFavouriteFilledImage = ResourcesCompat.getDrawable(
             context.resources,
             R.drawable.ic_favorite_filled,
-            null)
+            null
+        )
         private val icFavouriteBorderedImage = ResourcesCompat.getDrawable(
             context.resources,
             R.drawable.ic_favorite_bordered,
-            null)
+            null
+        )
 
         fun setData(city: City, position: Int) {
             binding.txvCityName.text = city.name
@@ -47,28 +52,45 @@ class CityAdapter(val context: Context, var cityList: ArrayList<City>): Adapter<
 
             if (city.isFavorite) {
                 binding.imvFavorite.setImageDrawable(icFavouriteFilledImage)
-
-                //mark as not favorite
-                binding.imvFavorite.setOnClickListener {
-                    Log.e("GloboTour","mark ${city.name} as not favorite")
-                    city.isFavorite = false
-                    binding.imvFavorite.setImageDrawable(icFavouriteBorderedImage)
-                    VacationSpots.favourableCities.remove(city)
-                }
             }else {
                 binding.imvFavorite.setImageDrawable(icFavouriteBorderedImage)
-
-                //mark as favorite
-                binding.imvFavorite.setOnClickListener {
-                    Log.e("GloboTour","mark ${city.name} as favorite")
-                    city.isFavorite = true
-                    binding.imvFavorite.setImageDrawable(icFavouriteFilledImage)
-                    VacationSpots.favourableCities.add(city)
-                }
             }
 
             this.currentCity = city
             this.currentPosition = position
+        }
+
+        fun setListeners() {
+            binding.imvFavorite.setOnClickListener(this)
+            binding.imvDelete.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            when (v!!.id) {
+                R.id.imv_delete -> removeCity()
+                R.id.imv_favorite -> toggleFavorite()
+            }
+        }
+
+        private fun toggleFavorite() {
+            currentCity?.isFavorite =
+                !(currentCity?.isFavorite!!) //toggle the isFavorite Boolean value
+
+            if (currentCity?.isFavorite!!) {
+                binding.imvFavorite.setImageDrawable(icFavouriteFilledImage)
+                VacationSpots.favourableCities.add(currentCity!!)
+            } else {
+                binding.imvFavorite.setImageDrawable(icFavouriteBorderedImage)
+                VacationSpots.favourableCities.remove(currentCity!!)
+            }
+        }
+
+        private fun removeCity() {
+            cityList.removeAt(currentPosition)
+            notifyItemRemoved(currentPosition)
+            notifyItemRangeChanged(currentPosition, cityList.size)
+
+            VacationSpots.favourableCities.remove(currentCity!!)
         }
     }
 
