c6dfeb36b813a9b21a851a7f245a63258d1810e9
njorojohn <johnnjoroge40@gmail.com>
2021-07-28 15:15:01 +0300

    use the recyclerview with the fragment

--- app/src/main/java/com/njoro/globotour/city/CityFragment.kt
+++ app/src/main/java/com/njoro/globotour/city/CityFragment.kt
@@ -5,56 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.njoro.globotour.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.njoro.globotour.databinding.FragmentCityBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CityFragment.newInstance] factory method to
- * create an instance of this fragment.
+ *
  */
 class CityFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_city, container, false)
+        val view = FragmentCityBinding.inflate(
+            inflater,
+            container,
+            false
+
+        ).root //inflater.inflate(R.layout.fragment_city, container, false)
+        setupRecyclerView(view)
+        return  view;
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CityFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CityFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setupRecyclerView(view: View) {
+        val context = requireContext()
+        val cityAdapter = CityAdapter(context, VacationSpots.cityList!!)
+
+        view.let {
+            val binding = FragmentCityBinding.bind(it)
+
+            binding.cityRecyclerView.setHasFixedSize(true)
+
+            val layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
+            binding.cityRecyclerView.layoutManager = layoutManager
+
+
+            binding.cityRecyclerView.adapter = cityAdapter
+
+
+        }
+
+
     }
+
 }
